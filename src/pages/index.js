36b7c67b64658e3c261615578af3e15318d8cdaf
Luke Bennett <hello@lukebennett.com.au>
2019-08-05 16:50:20 +1000

    Add components to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import Hero from '../components/Hero';
+import Why from '../components/Why';
+import Services from '../components/Services';
+import GiftVoucher from '../components/GiftVoucher';
+import Contact from '../components/Contact';
 import Map from '../components/Map';
 
 const IndexPage = () => (
@@ -10,6 +14,10 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Hero />
     <Layout>
+      <Why />
+      <Services />
+      <GiftVoucher />
+      <Contact />
       <Map />
     </Layout>
   </>
