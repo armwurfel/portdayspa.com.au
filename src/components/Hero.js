87442cc41d2d73bf8e2907837cb933527b6de8ca
Luke Bennett <hello@lukebennett.com.au>
2019-08-06 14:35:02 +1000

    Use marble image for header on mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Media from 'react-media';
 import { graphql, Link, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
 
@@ -7,7 +8,14 @@ import Logo from './Logo';
 const Hero = () => {
   const data = useStaticQuery(graphql`
     query HeroQuery {
-      file(relativePath: { eq: "hero.jpg" }) {
+      hero: file(relativePath: { eq: "hero.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 3000, quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      marble: file(relativePath: { eq: "marble.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 3000, quality: 100) {
             ...GatsbyImageSharpFluid_withWebp
@@ -29,11 +37,23 @@ const Hero = () => {
   return (
     <div className="relative">
       <div className="max-w-6xl ml-auto">
-        <Image
-          style={{ minHeight: `30rem`, height: `100%` }}
-          fluid={data.file.childImageSharp.fluid}
-          loading="eager"
-        />
+        <Media query="(min-width: 768px)">
+          {matches =>
+            matches ? (
+              <Image
+                style={{ minHeight: `30rem`, height: `100%` }}
+                fluid={data.hero.childImageSharp.fluid}
+                loading="eager"
+              />
+            ) : (
+              <Image
+                style={{ minHeight: `30rem`, height: `100%` }}
+                fluid={data.marble.childImageSharp.fluid}
+                loading="eager"
+              />
+            )
+          }
+        </Media>
       </div>
       <div className="absolute flex flex-col font-sans inset-0 px-16 py-8 text-gray-700">
         <div className="hidden md:block leading-none uppercase -mx-3 text-sm">
@@ -59,11 +79,11 @@ const Hero = () => {
             Booking
           </Link>
         </div>
-        <div className="flex flex-1 flex-col items-center justify-center max-w-sm mx-auto text-center">
-          <h1>
-            <Logo className="fill-current h-24" />
+        <div className="flex flex-1 flex-col items-center justify-center max-w-xl mx-auto text-center w-full">
+          <h1 className="w-full">
+            <Logo className="fill-current w-full" />
           </h1>
-          <p className="font-light leading-tight mt-8 mx-8 uppercase">
+          <p className="font-light leading-tight max-w-sm mt-8 mx-8 uppercase">
             Allow us to rejuvenate, <br />
             rehydrate and re-new
           </p>
