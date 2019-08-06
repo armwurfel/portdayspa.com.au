33d796b7d97b64e2fce35c7670bae574b1d9b504
Luke Bennett <hello@lukebennett.com.au>
2019-08-06 15:50:27 +1000

    Redo widget with react-icons

--- src/components/SocialWidgetMobile.js
+++ src/components/SocialWidgetMobile.js
@@ -1,38 +1,34 @@
 import React from 'react';
+import { MdEmail, MdPhone } from 'react-icons/md';
 import { graphql, useStaticQuery } from 'gatsby';
 
 import Facebook from './icons/Facebook';
-import Instagram from './icons/Instagram';
 
 const SocialWidgetMobile = () => {
   const data = useStaticQuery(graphql`
     query SocialWidgetMobileQuery {
       site {
         siteMetadata {
+          email
           facebook
           instagram
+          phone
+          phoneFormatted
         }
       }
     }
   `);
   return (
-    <div className="bg-white bottom-0 fixed flex items-end justify-center p-4 pointer-events-none w-screen z-50">
-      <div>
-        <a
-          href={data.site.siteMetadata.facebook}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Facebook className="bg-white fill-current pointer-events-auto rounded-full text-gray-700 hover:text-gray-600 w-6" />
+    <div className="bottom-0 pointer-events-none fixed w-full z-50">
+      <div className="bg-white flex items-end justify-around p-4">
+        <a href={`tel:${data.site.siteMetadata.phone}`}>
+          <MdPhone className="bg-white fill-current rounded-full text-gray-700 hover:text-gray-600 text-xl" />
         </a>
-      </div>
-      <div className="ml-2">
-        <a
-          href={data.site.siteMetadata.instagram}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Instagram className="bg-white fill-current pointer-events-auto rounded-full text-gray-700 hover:text-gray-600 w-6" />
+        <a href={data.site.siteMetadata.facebook}>
+          <Facebook className="bg-white fill-current rounded-full text-gray-700 hover:text-gray-600 text-xl" />
+        </a>
+        <a href={`mailto:${data.site.siteMetadata.email}`}>
+          <MdEmail className="bg-white fill-current rounded-full text-gray-700 hover:text-gray-600 text-xl" />
         </a>
       </div>
     </div>
