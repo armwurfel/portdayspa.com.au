03ee7fa791f842ca307241761e6f32f4c1293709
Luke Bennett <hello@lukebennett.com.au>
2019-08-13 09:38:42 +1000

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+
+import Layout from '../components/Layout';
+import Contact from '../components/Contact';
+import Map from '../components/Map';
+
+const ContactPage = ({ data }) => {
+  return (
+    <Layout>
+      <Contact data={data} />
+      <Map />
+    </Layout>
+  );
+};
+
+ContactPage.propTypes = {
+  data: PropTypes.object,
+};
+
+export default ContactPage;
+
+export const query = graphql`
+  query ContactPageQuery {
+    hero: file(relativePath: { eq: "contact-hero.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 3000, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        address
+        email
+        facebook
+        googleMapsLink
+        instagram
+        phone
+        phoneFormatted
+      }
+    }
+  }
+`;
