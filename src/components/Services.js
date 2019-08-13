dec2d5c25c4202cc23b1db7f5f7d8a2d43f60839
Luke Bennett <hello@lukebennett.com.au>
2019-08-13 12:21:51 +1000

    Use Services.js for Services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import { graphql, Link, useStaticQuery } from 'gatsby';
-import Image from 'gatsby-image';
+
+import Service from './utils/Service';
+import Background from '../images/background.svg';
 
 const Services = () => {
   const data = useStaticQuery(graphql`
     query ServicesQuery {
-      file(relativePath: { eq: "services.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 3000, quality: 100) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
-        }
-      }
       site {
         siteMetadata {
           title
@@ -21,61 +15,125 @@ const Services = () => {
     }
   `);
   return (
-    <ParallaxProvider>
-      <Parallax y={[-50, 50]}>
-        <div className="overflow-hidden">
-          <Image
-            style={{
-              minHeight: `30rem`,
-              height: `100%`,
-              maxHeight: `50rem`,
-              width: `100%`,
-            }}
-            fluid={data.file.childImageSharp.fluid}
-            loading="eager"
-          />
+    <div className="relative">
+      <div className="absolute">
+        <img className="h-full translate-left-50" src={Background} alt="" />
+      </div>
+      <div className="bg-cover flex flex-wrap py-4 relative">
+        <div className="font-semibold font-serif leading-tight px-12 py-6 text-2xl uppercase w-full md:w-1/3">
+          <h1>{data.site.siteMetadata.title} Services Menu</h1>
+          <ul className="list-dash ml-5 mt-4">
+            <li>
+              <Link
+                to="/services/skin"
+                activeClassName="underline"
+                partiallyActive
+              >
+                Skin
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/services/body"
+                activeClassName="underline"
+                partiallyActive
+              >
+                Body
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/services/beauty"
+                activeClassName="underline"
+                partiallyActive
+              >
+                Beauty
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/services/ipl-srh"
+                activeClassName="underline"
+                partiallyActive
+              >
+                IPL/SRH
+              </Link>
+            </li>
+          </ul>
         </div>
-      </Parallax>
-      <article
-        id="services"
-        className="bg-white px-4 md:px-6 py-24 relative w-full z-10"
-      >
-        <div className="flex flex-wrap max-w-6xl  mx-auto w-full">
-          <div className="flex items-center justify-center w-full md:w-2/5">
-            <h2 className="font-serif leading-tight md:p-10 text-2xl uppercase w-full">
-              {data.site.siteMetadata.title} Services
-            </h2>
-          </div>
-          <div className="md:col-3 mt-4 md:mt-0 w-full md:w-3/5">
-            <ul className="font-bold leading-none pr-12">
-              <li className="mb-4">Skin Consulation</li>
-              <li className="mb-4">Signature Faicals</li>
-              <li className="mb-4">Skin Workouts</li>
-              <li className="mb-4">Skin Needling</li>
-              <li className="mb-4">Radiance Plus</li>
-              <li className="mb-4">Paramedical skin treatments</li>
-              <li className="mb-4">Simultaneously</li>
-              <li className="mb-4">Hydro-Brasion</li>
-              <li className="mb-4">Spa Packages</li>
-              <li className="mb-4">MassagesBody</li>
-              <li className="mb-4">Beauty</li>
-              <li className="mb-4">Lash Lift</li>
-              <li className="mb-4">Waxing &amp; Tinting</li>
-              <li className="mb-4">Makeup</li>
-              <li className="mb-4">IPL/SHR</li>
-            </ul>
-          </div>
-          <div className="mt-8 text-center w-full">
-            <Link
-              to="/services"
-              className="border border-gray-500 inline-block leading-none px-2 py-1 uppercase"
+        <div className="px-12 w-2/3">
+          <div className="border px-4 py-6">
+            <div className="flex font-serif justify-between leading-tight uppercase">
+              <h2>Skin</h2>
+              <div>Price</div>
+            </div>
+            <Service
+              title="Skin Consultation*"
+              price={[
+                <p>$50*</p>,
+                <p className="mt-0">
+                  <small>*This cost is redeemable on products/treatments</small>
+                </p>,
+              ]}
             >
-              View more information here
-            </Link>
+              Our comprehensive Consultation process is designed to help you
+              create a customised treatment and homecare plan, allowing you to
+              address your skin concerns and reveal your best ever skin.
+            </Service>
+            <h3 className="font-bold mt-6 uppercase">Signature Facials</h3>
+            <Service title="Multi – Detox" price="$179 (90 mins)">
+              Revitalise, clarify, cleanse. The high potency multi-clay mask,
+              with kaolin & bentonite, is formulated to promote cell renewal,
+              detoxify and calm normal, combination and oily skin types. The
+              blend of active ingredients will ensure your skin reclaims a
+              healthy glow.
+            </Service>
+            <Service title="Age Delay" price="$170 (90 mins)">
+              An intensive illumination treatment that provides a powerful dose
+              of antioxidants and hydration. Skin is visibly firmer with a
+              smoother texture and tone. Sonophoresis combined with active
+              ingredients ensures deep penetration of products to target the
+              signs of ageing, dehydrated skin, photo damage, pigmentation and
+              poor skin texture.
+            </Service>
+            <Service title="Revitalising" price="$170 (90 mins)">
+              An intensive, revitalising cosmedical skincare treatment designed
+              to deep cleanse, decongest and replenish tired skin. Sonophoresis
+              will further penetrate active ingredients.
+            </Service>
+            <Service title="Ultra Calming" price="$170 (90 mins)">
+              A gentle, calming treatment that will hydrate, calm and visibly
+              reduce redness. This treatment combines lymphatic drainage through
+              the use of smooth microdermabrasion tip. Great for rosacea or
+              inflammatory skins.
+            </Service>
+            <Service title="Bio Hydra" price="$149 (45 mins)">
+              A treatment to stimulate the production of collagen & elastin,
+              restore moisture loss and provide clarity to the skin. This
+              gorgeous facial contains a unique formula of plant extracts and
+              hydraluronic acid to bind and lock in moisture.
+            </Service>
+            <Service
+              title="PDS Customised Signature Facial"
+              price="$49 (45 mins)"
+            >
+              This relaxing facial is customised to your skins individual
+              requirements. It includes deep cleanse, thorough exfoliation,
+              relaxing facial massage, customised mask, active customised
+              serums, moisture protectives and
+            </Service>
+            <p className="text-center">
+              <Link
+                className="border border-gray-500 inline-block leading-none px-2 py-1 uppercase"
+                to="/booking"
+              >
+                Book now
+              </Link>
+            </p>
           </div>
         </div>
-      </article>
-    </ParallaxProvider>
+      </div>
+    </div>
   );
 };
 
