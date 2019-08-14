da847ad1b1790d81ed607fbfd55a9ede5c6e8dc5
Luke Bennett <hello@lukebennett.com.au>
2019-08-14 13:15:09 +1000

    Install react-spring

--- src/components/ServicesMenu.js
+++ src/components/ServicesMenu.js
@@ -13,38 +13,55 @@ const ServicesMenu = () => {
   `);
 
   return (
-    <div className="font-serif leading-tight px-12 py-6 text-2xl uppercase w-full md:w-1/3">
-      <h1>{data.site.siteMetadata.title} Services Menu</h1>
-      <ul className="list-dash ml-5 mt-4">
-        <li>
-          <Link to="/services/skin" activeClassName="underline" partiallyActive>
-            Skin
-          </Link>
-        </li>
-        <li>
-          <Link to="/services/body" activeClassName="underline" partiallyActive>
-            Body
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/services/beauty"
-            activeClassName="underline"
-            partiallyActive
-          >
-            Beauty
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/services/ipl-srh"
-            activeClassName="underline"
-            partiallyActive
-          >
-            IPL/SRH
-          </Link>
-        </li>
-      </ul>
+    <div className="font-serif leading-tight text-2xl uppercase w-full md:w-1/3">
+      <div className="bg-white border px-12 py-6">
+        <h1>{data.site.siteMetadata.title} Services Menu</h1>
+        <ul className="mt-4">
+          <li className="mt-2">
+            <Link
+              to="/services/skin"
+              className="bg-white border border-gray-500 inline-block leading-none px-2 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white hover:border-gray-800"
+              activeClassName="underline"
+              partiallyActive
+            >
+              <div className="flex justify-between">
+                <span>Skin</span>
+                <span>></span>
+              </div>
+            </Link>
+          </li>
+          <li className="mt-2">
+            <Link
+              to="/services/body"
+              className="bg-white border border-gray-500 inline-block leading-none px-2 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white hover:border-gray-800"
+              activeClassName="underline"
+              partiallyActive
+            >
+              Body
+            </Link>
+          </li>
+          <li className="mt-2">
+            <Link
+              to="/services/beauty"
+              className="bg-white border border-gray-500 inline-block leading-none px-2 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white hover:border-gray-800"
+              activeClassName="underline"
+              partiallyActive
+            >
+              Beauty
+            </Link>
+          </li>
+          <li className="mt-2">
+            <Link
+              to="/services/ipl-srh"
+              className="bg-white border border-gray-500 inline-block leading-none px-2 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white hover:border-gray-800"
+              activeClassName="underline"
+              partiallyActive
+            >
+              IPL/SRH
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
