a0b178238f4332302323fd5580129f5df867b18c
Luke Bennett <hello@lukebennett.com.au>
2019-08-16 10:23:08 +1000

    Show different hero image on mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Media from 'react-media';
-import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+import { Parallax } from 'react-scroll-parallax';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
 
@@ -16,7 +16,7 @@ const Hero = () => {
           }
         }
       }
-      marble: file(relativePath: { eq: "marble.jpg" }) {
+      mobile: file(relativePath: { eq: "hero-mobile.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 3000, quality: 100) {
             ...GatsbyImageSharpFluid_withWebp
@@ -36,45 +36,49 @@ const Hero = () => {
     }
   `);
   return (
-    <ParallaxProvider>
-      <div className="overflow-hidden relative">
-        <div className="max-w-6xl ml-auto">
-          <Parallax y={[-75, 50]}>
-            <Media query="(min-width: 768px)">
-              {matches =>
-                matches ? (
-                  <Image
-                    style={{
-                      minHeight: `30rem`,
-                      height: `100%`,
-                    }}
-                    fluid={data.hero.childImageSharp.fluid}
-                    loading="eager"
-                  />
-                ) : (
-                  <Image
-                    style={{ minHeight: `30rem`, height: `100%` }}
-                    fluid={data.marble.childImageSharp.fluid}
-                    loading="eager"
-                  />
-                )
-              }
-            </Media>
-          </Parallax>
-        </div>
-        <div className="absolute flex flex-col font-sans inset-0 px-16 py-8 text-gray-700">
-          <div className="flex flex-1 flex-col items-center justify-center max-w-xl mx-auto text-center w-full">
-            <h1 className="w-full">
-              <Logo className="fill-current w-full" />
-            </h1>
-            <p className="font-light leading-tight max-w-sm mt-8 mx-8 uppercase">
-              Allow us to rejuvenate, <br />
-              rehydrate and re-new
-            </p>
-          </div>
+    <div className="overflow-hidden relative">
+      <div className="max-w-6xl ml-auto">
+        <Parallax y={[-75, 50]}>
+          <Media query="(min-width: 768px)">
+            {matches =>
+              matches ? (
+                <Image
+                  style={{
+                    minHeight: `30rem`,
+                    height: `100%`,
+                  }}
+                  fluid={data.hero.childImageSharp.fluid}
+                  loading="eager"
+                />
+              ) : (
+                <Image
+                  className="opacity-75"
+                  style={{
+                    minHeight: `30rem`,
+                    height: `100%`,
+                    width: `100%`,
+                    objectPosition: `right`,
+                  }}
+                  fluid={data.mobile.childImageSharp.fluid}
+                  loading="eager"
+                />
+              )
+            }
+          </Media>
+        </Parallax>
+      </div>
+      <div className="absolute flex flex-col font-sans inset-0 px-16 py-8 text-gray-700">
+        <div className="flex flex-1 flex-col items-center justify-center max-w-xl mx-auto text-center w-full">
+          <h1 className="w-full">
+            <Logo className="fill-current w-full" />
+          </h1>
+          <p className="font-light leading-tight max-w-sm mt-8 mx-8 uppercase">
+            Allow us to rejuvenate, <br />
+            rehydrate and re-new
+          </p>
         </div>
       </div>
-    </ParallaxProvider>
+    </div>
   );
 };
 
