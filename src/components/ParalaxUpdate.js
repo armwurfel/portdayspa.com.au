c609a13c4ae786ef5715cd1992fa6e73484d9eb2
mrhut10 <mrhut10@protonmail.com>
2019-08-19 03:04:31 +1000

    fix build time error of window not defined (nor parallaxController)

--- src/components/ParalaxUpdate.js
+++ src/components/ParalaxUpdate.js
@@ -3,7 +3,13 @@ import { withController } from 'react-scroll-parallax';
 import PropTypes from 'prop-types';
 
 const ParalaxUpdate = ({ children, parallaxController, location }) => {
-  useEffect(parallaxController.update, [parallaxController, location]);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      console.log('running parallax update');
+      parallaxController.update();
+      return ()=>{console.log("exiting parallax update")};
+    }
+  }, [parallaxController, location]);
   return <>{children}</>;
 };
 
