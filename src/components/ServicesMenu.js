545d1a10e0ccd73b8595d5bb57b62df98f5c6172
Luke Bennett <hello@lukebennett.com.au>
2019-08-19 17:32:37 +1000

    Create collapsable menu for services

--- src/components/ServicesMenu.js
+++ src/components/ServicesMenu.js
@@ -1,72 +1,75 @@
 import React, { useState, useEffect } from 'react';
-import { graphql, Link, useStaticQuery } from 'gatsby';
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
 
-const ServicesMenu = () => {
-  const [isActive, toggleActive] = useState(false);
+const ServicesMenu = ({ service }) => {
+  const [isActive, toggleIsActive] = useState(false);
+  const [isOpen, toggleIsOpen] = useState(false);
 
   useEffect(() => {
     if (window.location.pathname === '/services') {
-      toggleActive(true);
+      toggleIsActive(true);
     }
   }, []);
 
-  const data = useStaticQuery(graphql`
-    query ServicesTemplateQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
-    <div className="font-serif leading-tight text-2xl uppercase w-full md:w-1/3">
-      <div className="bg-white border px-12 py-6">
-        <h1>{data.site.siteMetadata.title} Services Menu</h1>
-        <ul className="mt-4">
-          <li className="mt-2">
-            <Link
-              to="/services/skin"
-              className={`${
-                isActive ? 'bg-gray-800 text-white border-gray-800' : null
-              } bg-white border border-gray-500 flex items-center justify-between leading-none px-2 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white hover:border-gray-800`}
-              activeClassName="bg-gray-800 text-white border-gray-800"
-            >
-              Skin
-            </Link>
-          </li>
-          <li className="mt-2">
-            <Link
-              to="/services/body"
-              className="bg-white border border-gray-500 flex items-center justify-between leading-none px-2 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white hover:border-gray-800"
-              activeClassName="bg-gray-800 text-white border-gray-800"
-            >
-              Body
-            </Link>
-          </li>
-          <li className="mt-2">
-            <Link
-              to="/services/beauty"
-              className="bg-white border border-gray-500 flex items-center justify-between leading-none px-2 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white hover:border-gray-800"
-              activeClassName="bg-gray-800 text-white border-gray-800"
-            >
-              Beauty
-            </Link>
-          </li>
-          <li className="mt-2">
-            <Link
-              to="/services/ipl-srh"
-              className="bg-white border border-gray-500 flex items-center justify-between leading-none px-2 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white hover:border-gray-800"
-              activeClassName="bg-gray-800 text-white border-gray-800"
-            >
-              IPL/SRH
-            </Link>
-          </li>
-        </ul>
+    <div className="font-serif leading-tight text-2xl uppercase">
+      <div className="flex justify-between px-4 py-2">
+        <h1>{service}</h1>
+        <button
+          onClick={() => toggleIsOpen(!isOpen)}
+          className="border border-gray-500 font-light font-sans inline-block px-2 py-1 text-sm transition-all uppercase hover:bg-gray-800 hover:text-white focus:outline-none focus:bg-gray-800 focus:border-gray-800 focus:text-white"
+          type="button"
+        >
+          Menu
+        </button>
       </div>
+      <ul className={`${isOpen ? '' : 'hidden'}`}>
+        <li className="border-t last:border-b hover:border-gray-800">
+          <Link
+            to="/services/skin"
+            className={`${
+              isActive ? 'bg-gray-800 text-white border-gray-800' : ''
+            } flex items-center justify-between leading-none px-4 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white`}
+            activeClassName="bg-gray-800 text-white border-gray-800"
+          >
+            Skin
+          </Link>
+        </li>
+        <li className="border-t last:border-b hover:border-gray-800">
+          <Link
+            to="/services/body"
+            className="flex items-center justify-between leading-none px-4 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white"
+            activeClassName="bg-gray-800 text-white border-gray-800"
+          >
+            Body
+          </Link>
+        </li>
+        <li className="border-t last:border-b hover:border-gray-800">
+          <Link
+            to="/services/beauty"
+            className="flex items-center justify-between leading-none px-4 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white"
+            activeClassName="bg-gray-800 text-white border-gray-800"
+          >
+            Beauty
+          </Link>
+        </li>
+        <li className="border-t last:border-b hover:border-gray-800">
+          <Link
+            to="/services/ipl-srh"
+            className="flex items-center justify-between leading-none px-4 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white"
+            activeClassName="bg-gray-800 text-white border-gray-800"
+          >
+            IPL/SRH
+          </Link>
+        </li>
+      </ul>
     </div>
   );
+};
+
+ServicesMenu.propTypes = {
+  service: PropTypes.string,
 };
 
 export default ServicesMenu;
