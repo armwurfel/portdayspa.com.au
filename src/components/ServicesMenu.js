7f877f0d50c38e08651f0ff118ccda2bd17a221d
Luke Bennett <hello@lukebennett.com.au>
2019-08-20 13:01:24 +1000

    Style menu

--- src/components/ServicesMenu.js
+++ src/components/ServicesMenu.js
@@ -13,9 +13,9 @@ const ServicesMenu = ({ service }) => {
   }, []);
 
   return (
-    <div className="font-serif leading-tight text-2xl uppercase">
-      <div className="flex justify-between px-4 py-2">
-        <h1>{service}</h1>
+    <div className="leading-tight uppercase">
+      <div className="flex items-center justify-between leading-none px-4 py-4">
+        <h1 className="font-serif text-4xl">{service}</h1>
         <button
           onClick={() => toggleIsOpen(!isOpen)}
           className="border border-gray-500 font-light font-sans inline-block px-2 py-1 text-sm transition-all uppercase hover:bg-gray-800 hover:text-white focus:outline-none focus:bg-gray-800 focus:border-gray-800 focus:text-white"
@@ -25,40 +25,40 @@ const ServicesMenu = ({ service }) => {
         </button>
       </div>
       <ul className={`${isOpen ? '' : 'hidden'}`}>
-        <li className="border-t last:border-b hover:border-gray-800">
+        <li className="border-t last:border-b border-gray-200 hover:border-gray-800">
           <Link
             to="/services/skin"
             className={`${
               isActive ? 'bg-gray-800 text-white border-gray-800' : ''
-            } flex items-center justify-between leading-none px-4 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white`}
-            activeClassName="bg-gray-800 text-white border-gray-800"
+            } flex items-center justify-between leading-none p-4 uppercase transition-all w-full hover:bg-gray-800 hover:text-white`}
+            activeClassName="bg-gray-100 border-gray-100"
           >
             Skin
           </Link>
         </li>
-        <li className="border-t last:border-b hover:border-gray-800">
+        <li className="border-t last:border-b border-gray-200 hover:border-gray-800">
           <Link
             to="/services/body"
-            className="flex items-center justify-between leading-none px-4 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white"
-            activeClassName="bg-gray-800 text-white border-gray-800"
+            className="flex items-center justify-between leading-none p-4 uppercase transition-all w-full hover:bg-gray-800 hover:text-white"
+            activeClassName="bg-gray-100 border-gray-100"
           >
             Body
           </Link>
         </li>
-        <li className="border-t last:border-b hover:border-gray-800">
+        <li className="border-t last:border-b border-gray-200 hover:border-gray-800">
           <Link
             to="/services/beauty"
-            className="flex items-center justify-between leading-none px-4 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white"
-            activeClassName="bg-gray-800 text-white border-gray-800"
+            className="flex items-center justify-between leading-none p-4 uppercase transition-all w-full hover:bg-gray-800 hover:text-white"
+            activeClassName="bg-gray-100 border-gray-100"
           >
             Beauty
           </Link>
         </li>
-        <li className="border-t last:border-b hover:border-gray-800">
+        <li className="border-t last:border-b border-gray-200 hover:border-gray-800">
           <Link
             to="/services/ipl-srh"
-            className="flex items-center justify-between leading-none px-4 py-1 uppercase transition-all w-full hover:bg-gray-800 hover:text-white"
-            activeClassName="bg-gray-800 text-white border-gray-800"
+            className="flex items-center justify-between leading-none p-4 uppercase transition-all w-full hover:bg-gray-800 hover:text-white"
+            activeClassName="bg-gray-100 border-gray-100"
           >
             IPL/SHR
           </Link>
