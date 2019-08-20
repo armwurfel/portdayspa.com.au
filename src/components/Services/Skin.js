39db356525032f081e83c2f55f5e297cbc9aec82
Luke Bennett <hello@lukebennett.com.au>
2019-08-20 12:39:41 +1000

    Add correct services

--- src/components/Services/Skin.js
+++ src/components/Services/Skin.js
@@ -6,55 +6,163 @@ import ServicesMenu from '../ServicesMenu';
 const Skin = () => {
   return (
     <>
-      <ServicesMenu service="skin" />
+      <ServicesMenu service="Skin" />
       <div className="px-4">
-        <Service title="Skin Consultation*" price={[<p>$50*</p>]}>
-          Our comprehensive Consultation process is designed to help you create
-          a customised treatment and homecare plan, allowing you to address your
-          skin concerns and reveal your best ever skin.
+        <h3 className="font-bold mt-6 uppercase">
+          Ultraceuticals Signature Facials
+        </h3>
+        <p>
+          Ultraceuticals luxurious signature facial treatments incorporate
+          potent active ingredients, UltraSonophoresis and massage to treat key
+          skin concerns including premature skin ageing, pigmentation/uneven
+          skin tone, dehydration and congested skin.
+        </p>
+        <Service title="Vita-CLEAR Facial" price="$150" time="60mins">
+          <p>
+            This deep cleansing treatment incorporated BHA and potent
+            antioxidants to penetrate deep into the pores to clear congestion.
+            Works to calm inflamed skin, help control excessive oiliness,
+            mattify the complexion and minimise future skin breakouts.
+          </p>
           <p className="mt-0">
-            <small>*This cost is redeemable on products/treatments</small>
+            <small>*45min - $115, 30min - $80</small>
           </p>
         </Service>
-        <h3 className="font-bold mt-6 uppercase">Signature Facials</h3>
-        <Service title="Multi – Detox" price="$179" time="(90 mins)">
-          Revitalise, clarify, cleanse. The high potency multi-clay mask, with
-          kaolin &amp; bentonite, is formulated to promote cell renewal,
-          detoxify and calm normal, combination and oily skin types. The blend
-          of active ingredients will ensure your skin reclaims a healthy glow.
+        <Service title="Vita-REPAIR Facial" price="$150" time="60mins">
+          <p>
+            Works to repair stressed and dehydrated skin, this essential
+            moisture boost treatment balances and restores skin's natural
+            hydration levels. Features Ultraceuticals' Vitamin B2 moisturising
+            complex (Provitamin B5 and Vitamin B3) to visibly nourish skin and
+            plump fine lines and wrinkles.
+          </p>
+          <p className="mt-0">
+            <small>*45min - $115, 30min - $80</small>
+          </p>
         </Service>
-        <Service title="Age Delay" price="$170" time="(90 mins)">
-          An intensive illumination treatment that provides a powerful dose of
-          antioxidants and hydration. Skin is visibly firmer with a smoother
-          texture and tone. Sonophoresis combined with active ingredients
-          ensures deep penetration of products to target the signs of ageing,
-          dehydrated skin, photo damage, pigmentation and poor skin texture.
+        <Service title="Vita-GLOW Facial" price="$150" time="60mins">
+          <p>
+            This multi-action facial visibly reduces the appearance of
+            pigmentation, uneven skin tone and dark spots. A powerful
+            combination of 5 hyperpigment blockers, skin brighteners and
+            exfoliants to synergistically restore skin luminosity and radiance.
+          </p>
+          <p className="mt-0">
+            <small>*45min - $115, 30min - $80</small>
+          </p>
         </Service>
-        <Service title="Revitalising" price="$170" time="(90 mins)">
-          An intensive, revitalising cosmedical skincare treatment designed to
-          deep cleanse, decongest and replenish tired skin. Sonophoresis will
-          further penetrate active ingredients.
+        <Service title="Vita-LIFT ACE Power Facial" price="$150" time="60mins">
+          <p>
+            Featuring our triple-action anti-ageing powerhouse ingredients
+            Vitamin A, Vitamin C and AHAs & BHA, this treatment combines pure
+            indulgence with skin rejuvenation. Actively works to reverse the
+            visible signs of ageing by perfecting and refining the skins overall
+            appearance.
+          </p>
         </Service>
-        <Service title="Ultra Calming" price="$170" time="(90 mins)">
-          A gentle, calming treatment that will hydrate, calm and visibly reduce
-          redness. This treatment combines lymphatic drainage through the use of
-          smooth microdermabrasion tip. Great for rosacea or inflammatory skins.
+        <h3 className="font-bold mt-6 uppercase">
+          Ultraceuticals Skin Workouts
+        </h3>
+        <p>
+          Skin Workout treatments (often referred to as skin peels) aim to
+          rejuvenate skin through accelerated exfoliation. These treatments have
+          been developed to quickly refresh and restore skins youthful
+          appearance.
+        </p>
+        <Service title="Vita-CLEAR Skin Workout" price="$95" time="30mins">
+          <p>
+            This Mandelic Acid Peel utilises AHAs to quickly decongest, clarify
+            and help control oil production. With proven anti-ageing ingredient
+            Niacinamide and potent antioxidant Clove Oil, this peel improves
+            surface congestion and reduces inflammation.
+          </p>
+          <p className="mt-0">
+            <small>*45min + Infusion $130</small>
+          </p>
         </Service>
-        <Service title="Bio Hydra" price="$149" time="(45 mins)">
-          A treatment to stimulate the production of collagen &amp; elastin,
-          restore moisture loss and provide clarity to the skin. This gorgeous
-          facial contains a unique formula of plant extracts and hydraluronic
-          acid to bind and lock in moisture.
+        <Service title="Vita-REPAIR Skin Workout" price="$90" time="30mins">
+          <p>
+            Reveal refreshed, youthful, radiant skin in just one treatment. This
+            high-powered Lactic Acid peel delivers fast results and actively
+            works to resurface and improve skins moisture levels while restoring
+            clarity and luminosity.
+          </p>
+          <p className="mt-0">
+            <small>*45min + Infusion $125</small>
+          </p>
         </Service>
-        <Service
-          title="PDS Customised Signature Facial"
-          price="$49"
-          time="(45 mins)"
-        >
-          This relaxing facial is customised to your skins individual
-          requirements. It includes deep cleanse, thorough exfoliation, relaxing
-          facial massage, customised mask, active customised serums, moisture
-          protectives and
+        <Service title="Vita-GLOW Skin Workout" price="$100" time="30mins">
+          <p>
+            Ideal treatment for ageing or sun damaged skin, this potent Vitapeel
+            features high-strength AHAs &amp; BHA. This skin brightening
+            treatment works to rapidly exfoliate surface congestion while
+            diminishing the appearance of dark spots, hyperpigmentation and
+            blotchiness to reveal a brighter and clearer complexion.
+          </p>
+          <p className="mt-0">
+            <small>*45min + Infusion $125</small>
+          </p>
+        </Service>
+        <Service title="A-Zyme Skin Workout" price="$165" time="30mins">
+          <p>
+            The powerhouse of all anti-ageing treatments. This fast-acting non
+            invasive skin rejuevenation treatment combines proven anti-ageing
+            ingredients Retinol and Bromelain to refresh, refine, hydrate and
+            plump skin. Works quickly, to dramatically diminish the appearance
+            of fine lines and wrinkles, even skin tone, and improve overall skin
+            luminosity.
+          </p>
+          <p className="mt-0">
+            <small>*45min + Infusion $200</small>
+          </p>
+        </Service>
+        <Service title="Radiance Renew Workout" price="$160" time="60mins">
+          <p>
+            This ultimate 3-1 skin workout treatment is designed to refine
+            pores, smooth coarse texture and help renew skins’ dewy youthful
+            glow. This powerful treatment combines Mandelic Acid and our
+            radiance boosting mask followed by an AHA, BHA infusion to help
+            smooth and restore overall skin luminosity. A series of at least 3
+            treatments are recommended for optimum results.
+          </p>
+        </Service>
+        <h3 className="font-bold mt-6 uppercase">
+          Ultraceuticals Paramedical treatments
+        </h3>
+        <p>
+          Performed by highly trained clinicians these treatments provide
+          remarkable regenerative effects on the skin and allow for maximum
+          results.
+        </p>
+        <Service title="Vita-Brasion Treatment" price="$150" time="45mins">
+          <p>
+            Total skin treatment incorporating deep exfoliation and Vitamin
+            infusion (UltraSonophoresis) of potent anti-ageing and antioxidant
+            Vitamin A &amp; C to refine, smooth plump the complexion.
+          </p>
+        </Service>
+        <Service title="Hydro-Brasion" price="$175" time="45mins">
+          <p>
+            Simultaneously incorporates skin resurfacing and Vitamin infusion
+            technology (Ultrasonophoresis) to deliver potent ant-ageing
+            ingredients to help smooth, plump and boost skin’s hydration levels.
+          </p>
+        </Service>
+        <Service title="Radiance Plus+" price="$120" time="30mins">
+          <p>
+            This radiance-boosting mask combines a powerful blend of 8 skin
+            brightening agents to rapidly reduce the appearance of
+            hyperpigmentation, dark spots and blotchiness to instantly reveal
+            luminous complexion.
+          </p>
+        </Service>
+        <Service title="Skin Needling (Face)" price="$349">
+          <p>
+            This radiance-boosting mask combines a powerful blend of 8 skin
+            brightening agents to rapidly reduce the appearance of
+            hyperpigmentation, dark spots and blotchiness to instantly reveal
+            luminous complexion.
+          </p>
         </Service>
       </div>
     </>
