e42ecd8f88ade3db592a865fdb59a6aaa1b213ff
Luke Bennett <hello@lukebennett.com.au>
2019-08-23 09:47:58 +1000

    Add gift voucher page

---
+++ src/pages/gift-voucher.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+import Layout from '../components/Layout';
+
+const GiftVoucher = () => {
+  useEffect(() => {
+    (function(g, i, f, t, u, p) {
+      i = g.createElement('script');
+      f = g.getElementsByTagName('script')[0];
+      i.async = 1;
+      i.src = 'https://cdn.giftup.app/dist/gift-up.js';
+      f.parentNode.insertBefore(i, f);
+    })(document);
+  }, []);
+  return (
+    <Layout>
+      <div
+        className="gift-up-target"
+        data-site-id="a0115535-5c3d-4925-b7cb-332e007a80c5"
+        data-platform="Other"
+      />
+    </Layout>
+  );
+};
+
+export default GiftVoucher;
