eda58a38de728dffbbcd6b09446212a7c8580c30
Luke Bennett <hello@lukebennett.com.au>
2019-08-26 09:23:17 +1000

    Change copy on button

--- src/components/GiftVoucher.js
+++ src/components/GiftVoucher.js
@@ -62,7 +62,7 @@ const GiftVoucher = () => {
                   to="/gift-voucher"
                   className="border border-gray-500 inline-block leading-none px-2 py-1 uppercase transition-all hover:bg-gray-800 hover:text-white hover:border-gray-800"
                 >
-                  Order a gift voucher
+                  Buy gift voucher
                 </Link>
               </p>
             </div>
