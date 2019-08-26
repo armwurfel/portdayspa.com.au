20b652df86a4b8e4045b9e8a704f992fea2fc2bc
Luke Bennett <hello@lukebennett.com.au>
2019-08-26 10:54:37 +1000

    Change order

--- src/components/Services/IPL-SHR.js
+++ src/components/Services/IPL-SHR.js
@@ -20,42 +20,6 @@ const IPLSHR = () => {
     <>
       <ServicesMenu service="IPL/SHR" />
       <div className="px-4">
-        <h3 className="font-bold mt-6 uppercase">IPL Skin Rejuvenation</h3>
-        <Service title="Skin Tone Correction" price="POA">
-          <p>
-            Whether born with them or acquired over the course of a lifetime,
-            virtually everyone has one or more sun spots, freckles or skin
-            discolouration they'd love to get rid of! A doctor's letter
-            confirming that no skin cancers are present is usually required
-            before commencing your treatment cycles.
-          </p>
-        </Service>
-        <Service title="Rejuvenation Treatment" price="POA">
-          <p>
-            Everybody loves the sunshine! However, in the long term, sun
-            exposure decreases the production of collagen, which in turn causes
-            damage to the skin making it feel wrinkled and coarse and looking
-            red and tired. Skin rejuvenation treatments will improve skin tone,
-            texture, refine and refresh the skin by increasing the production of
-            collagen in the skin.
-          </p>
-        </Service>
-        <Service title="Redness Reduction" price="POA">
-          <p>
-            Our trained clinicians can offer treatments which will reduce facial
-            blemishes such as red flushing and red spots found on cheeks and
-            around the nose with minimal downtime or risk.
-          </p>
-        </Service>
-        <Service title="Skin Clearing" price="POA">
-          <p>
-            Our trained clinicians are now able to offer IPL based acne
-            management programs. These unique treatments are designed to
-            specifically slow the overall production of oil that leads to acne,
-            thus dramatically reducing the chance of spots and minimising the
-            appearance of pores in general.
-          </p>
-        </Service>
         <h3 className="font-bold mt-6 uppercase">
           SuperLUX SHR IPL Permanent Hair Removal
         </h3>
@@ -117,6 +81,42 @@ const IPLSHR = () => {
             Click here to view our Spa Information &amp; Conditions
           </Link>
         </p>
+        <h3 className="font-bold mt-6 uppercase">IPL Skin Rejuvenation</h3>
+        <Service title="Skin Tone Correction" price="POA">
+          <p>
+            Whether born with them or acquired over the course of a lifetime,
+            virtually everyone has one or more sun spots, freckles or skin
+            discolouration they'd love to get rid of! A doctor's letter
+            confirming that no skin cancers are present is usually required
+            before commencing your treatment cycles.
+          </p>
+        </Service>
+        <Service title="Rejuvenation Treatment" price="POA">
+          <p>
+            Everybody loves the sunshine! However, in the long term, sun
+            exposure decreases the production of collagen, which in turn causes
+            damage to the skin making it feel wrinkled and coarse and looking
+            red and tired. Skin rejuvenation treatments will improve skin tone,
+            texture, refine and refresh the skin by increasing the production of
+            collagen in the skin.
+          </p>
+        </Service>
+        <Service title="Redness Reduction" price="POA">
+          <p>
+            Our trained clinicians can offer treatments which will reduce facial
+            blemishes such as red flushing and red spots found on cheeks and
+            around the nose with minimal downtime or risk.
+          </p>
+        </Service>
+        <Service title="Skin Clearing" price="POA">
+          <p>
+            Our trained clinicians are now able to offer IPL based acne
+            management programs. These unique treatments are designed to
+            specifically slow the overall production of oil that leads to acne,
+            thus dramatically reducing the chance of spots and minimising the
+            appearance of pores in general.
+          </p>
+        </Service>
       </div>
     </>
   );
