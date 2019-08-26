be4dd1cc9f92e52f67838c57aa516e42f48f670e
Luke Bennett <hello@lukebennett.com.au>
2019-08-26 10:22:04 +1000

    Update pricing and copy

--- src/components/Services/Beauty.js
+++ src/components/Services/Beauty.js
@@ -12,115 +12,115 @@ const Beauty = () => {
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Full Leg Wax</h4>
-            <p>&nbsp;-&nbsp;$52</p>
+            <p>&nbsp;-&nbsp;$56</p>
           </div>
-          <p className="mt-0">inc. g-string - $79</p>
-          <p className="mt-0">inc. repeat brazilian - $89</p>
+          <p className="mt-0">inc. g-string - $82</p>
+          <p className="mt-0">inc. repeat brazilian - $92</p>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Lower Half Leg</h4>
-            <p>&nbsp;-&nbsp;$26</p>
+            <p>&nbsp;-&nbsp;$28</p>
           </div>
-          <p className="mt-0">inc. bikini - $49</p>
-          <p className="mt-0">inc. g-string - $57</p>
-          <p className="mt-0">inc. repeat brazilian - $69</p>
+          <p className="mt-0">inc. bikini - $52</p>
+          <p className="mt-0">inc. g-string - $59</p>
+          <p className="mt-0">inc. repeat brazilian - $72</p>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">
               Brazilian <span className="font-normal">(1st time)</span>
             </h4>
-            <p>&nbsp;-&nbsp;$65</p>
+            <p>&nbsp;-&nbsp;$67</p>
           </div>
-          <p className="mt-0">Repeat within 4 weeks - $57</p>
+          <p className="mt-0">Repeat within 4 weeks - $60</p>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">G-string</h4>
-            <p>&nbsp;-&nbsp;$42</p>
+            <p>&nbsp;-&nbsp;$49</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Bikini</h4>
-            <p>&nbsp;-&nbsp;$25</p>
+            <p>&nbsp;-&nbsp;$29</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Face</h4>
-            <p>&nbsp;-&nbsp;$39</p>
+            <p>&nbsp;-&nbsp;$42</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Eyebrow shape</h4>
-            <p>&nbsp;-&nbsp;$20</p>
+            <p>&nbsp;-&nbsp;$22</p>
           </div>
-          <p>inc. lip - $29</p>
+          <p>inc. lip - $33</p>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Chin</h4>
-            <p>&nbsp;-&nbsp;$18</p>
+            <p>&nbsp;-&nbsp;$20</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Lip</h4>
-            <p>&nbsp;-&nbsp;$15</p>
+            <p>&nbsp;-&nbsp;$19</p>
           </div>
-          <p>inc. chin - $27</p>
+          <p>inc. chin - $29</p>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Half Arm</h4>
-            <p>&nbsp;-&nbsp;$28</p>
+            <p>&nbsp;-&nbsp;$32</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Full Arm</h4>
-            <p>&nbsp;-&nbsp;$35</p>
+            <p>&nbsp;-&nbsp;$36</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Chest</h4>
-            <p>&nbsp;-&nbsp;$40</p>
+            <p>&nbsp;-&nbsp;$45</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Back</h4>
-            <p>&nbsp;-&nbsp;$40</p>
+            <p>&nbsp;-&nbsp;$45</p>
           </div>
         </div>
         <h3 className="font-bold mt-6 uppercase">Tinting</h3>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Eyebrow</h4>
-            <p>&nbsp;-&nbsp;$19</p>
+            <p>&nbsp;-&nbsp;$22</p>
           </div>
           <p>
-            inc. brow shape - $35
+            inc. brow shape - $39
             <br />
-            inc. lash tint + brow shape - $45
+            inc. lash tint + brow shape - $49
           </p>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Henna Brows</h4>
-            <p>&nbsp;-&nbsp;$55</p>
+            <p>&nbsp;-&nbsp;$59</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Eyelash</h4>
-            <p>&nbsp;-&nbsp;$20</p>
+            <p>&nbsp;-&nbsp;$22</p>
           </div>
-          <p>inc. brow tint - $35</p>
+          <p>inc. brow tint - $39</p>
         </div>
         <h3 className="font-bold mt-6 uppercase">Spray tan</h3>
         <p>Your choice of colour</p>
@@ -136,31 +136,30 @@ const Beauty = () => {
           We know how important it is to look your absolute best on your special
           day that's why we choose to use Jane Iredale Skincare Makeup.
           Providing a long lasting flawless finish for all skin types and
-          conditions creating the look you desire. Choose to visit us at the spa
-          or we can come to you for a small fee.
+          conditions creating the look you desire.
         </p>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Student Makeup</h4>
-            <p>&nbsp;-&nbsp;$70</p>
+            <p>&nbsp;-&nbsp;$75</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Trial Makeup</h4>
-            <p>&nbsp;-&nbsp;$60</p>
+            <p>&nbsp;-&nbsp;$65</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Special Occasion</h4>
-            <p>&nbsp;-&nbsp;$90</p>
+            <p>&nbsp;-&nbsp;$95</p>
           </div>
         </div>
         <div className="mt-2">
           <div className="flex">
             <h4 className="font-bold">Bridal Makeup</h4>
-            <p>&nbsp;-&nbsp;$90</p>
+            <p>&nbsp;-&nbsp;$95</p>
           </div>
         </div>
         <h3 className="font-bold mt-6 uppercase">Lash Lift</h3>
