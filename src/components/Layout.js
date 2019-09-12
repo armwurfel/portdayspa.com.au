d0eeca79ab52d17c2cb7d83008cfdb1c7397b587
Luke Bennett <hello@lukebennett.com.au>
2019-09-12 10:21:31 +1000

    Wrap root element with ParallaxProvider

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Media from 'react-media';
 import PropTypes from 'prop-types';
 
-import { ParallaxProvider } from 'react-scroll-parallax';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import SocialWidgetDesktop from './SocialWidgetDesktop';
@@ -10,6 +9,7 @@ import SocialWidgetDesktop from './SocialWidgetDesktop';
 const Layout = ({ children }) => (
   <ParallaxProvider>
     <div className="antialiased flex flex-col font-sans leading-relaxed min-h-screen text-gray-700">
+  <div className="antialiased flex flex-col font-sans leading-relaxed min-h-screen text-gray-700">
       <Navbar />
       <main className="bg-white flex flex-1 flex-col mx-auto relative w-full">
         {children}
@@ -19,7 +19,14 @@ const Layout = ({ children }) => (
         {matches => (matches ? <SocialWidgetDesktop /> : null)}
       </Media>
     </div>
-  </ParallaxProvider>
+    <main className="bg-white flex flex-1 flex-col mx-auto relative w-full">
+      {children}
+    </main>
+    <Footer />
+    <Media query="(min-width: 768px)">
+      {matches => (matches ? <SocialWidgetDesktop /> : null)}
+    </Media>
+  </div>
 );
 
 Layout.propTypes = {
